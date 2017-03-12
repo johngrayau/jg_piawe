0e62bf0b86bb4ac0b6886c8452864bd0677a1710
John Gray <johngray.au+git@gmail.com>
2017-03-13 10:59:44 +1100

    working on tests

--- spec/piawe_spec.rb
+++ spec/piawe_spec.rb
@@ -4,8 +4,26 @@ describe Piawe do
   it 'has a version number' do
     expect(Piawe::VERSION).not_to be nil
   end
-
-  # it 'does something useful' do
-  #   expect(false).to eq(true)
-  # end
 end
+
+
+
+describe Piawe::Person, :private do
+
+	subject do
+		Piawe::Person.played_by (	{
+			"name" =>  "Ebony Boycott",
+			"hourlyRate" =>  75.0030,
+			"overtimeRate" =>  150.0000,
+			"normalHours" =>  35.0,
+			"overtimeHours" =>  7.3,
+			"injuryDate" =>  "2016/05/01" 
+		} )
+	end
+
+	it { is_expected.to respond_to(:name) }
+	it { is_expected.to respond_to(:hourly_rate) }
+	it { is_expected.to respond_to(:overtime_rate) }
+
+end
+
